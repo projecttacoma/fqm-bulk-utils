e1113895ab6faa7a592f11a673b8969883c6952d
LaurenD <laurend@mitre.org>
2025-01-31 14:37:35 -0500

    Initial conditional group creation
    Has some TODOs, dependent on fqm-execution branch

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@
 import { Command } from 'commander';
 import { cliDataRequirements } from './dataRequirements';
 import { cliBulkQueries } from './bulkQueries';
+import { cliGroup } from './group';
 
 const program = new Command();
 program.name('fqm-bulk-utils').description('FQM Bulk Utils');
@@ -17,5 +18,11 @@ program
   .argument('<measureBundle>', 'FHIR Measure Bundle path')
   .description("outputs bulk queries based on the passed measure's data requirements")
   .action(cliBulkQueries);
+
+program
+  .command('group')
+  .argument('<measureBundle>', 'FHIR Measure Bundle path')
+  .description("outputs a conditional group based on the passed measure's IPP definition")
+  .action(cliGroup);
 
 program.parse();
